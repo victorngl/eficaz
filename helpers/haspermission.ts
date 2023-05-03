3cf9cce971d53f334dddb5b75b8449059935c549
Joao Victor Costa <victor_ngl2@hotmail.com>
2023-05-03 10:37:55 -0300

    testando build

--- helpers/haspermission.ts
+++ helpers/haspermission.ts
@@ -1,10 +1,6 @@
 export const userHasPermission = (role_name: string, roles) => {
 
-   
-
     var filteredRoles = roles.filter(role => (role === role_name));
-
-    console.log(filteredRoles)
 
     if (filteredRoles.length >= 1 && role_name === filteredRoles[0]) {
 
